cd3deb52ec3729c2620baa5dbf450d0c13023020
grahammiller <grahammiller@localhost>
2021-04-17 02:44:10 -0700

    Added working multi-threaded server

--- src/main/java/shutdownOnLan/Server2.java
+++ src/main/java/shutdownOnLan/Server2.java
@@ -1,5 +1,42 @@
 package shutdownOnLan;
 
-public class Server2 {
+import java.io.*; 
+import java.text.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Stream;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths; 
+import javafx.scene.control.TextArea;
 
-}
+public class Server2  
+{ 
+    public static void main() throws IOException  
+    { 
+       	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+    	LocalDateTime now = LocalDateTime.now(); 
+        ServerSocket ss = new ServerSocket(888); 
+        System.out.println("Server Started");
+        while (true)  
+        { 
+            Socket s = null; 
+              
+            try 
+            { 
+                s = ss.accept(); 
+                DataInputStream dis = new DataInputStream(s.getInputStream()); 
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+                System.out.println("[LOG]"+ now +": Incoming Connection" + s.getInetAddress()); 
+                Thread t = new ClientHandler(s, dis, dos); 
+                t.start(); 
+            } 
+            catch (Exception e){ 
+                s.close(); 
+            } 
+        } 
+    } 
+} 
+ 
---
+++ src/main/java/shutdownOnLan/serverLauncher.java
@@ -0,0 +1,17 @@
+package shutdownOnLan;
+
+import java.io.IOException;
+
+public class serverLauncher {
+
+	public static void main(String[] args) {
+        try {
+			Server2.main();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
