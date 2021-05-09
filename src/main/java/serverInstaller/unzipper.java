a0705637cc9d49bdcc129aa6117004331ad8b200
LanaDelSlay <42992759+LanaDelSlay@users.noreply.github.com>
2021-05-09 02:25:48 -0700

    Automated taskScheduler
    Automated taskScheduler

---
+++ src/main/java/serverInstaller/unzipper.java
@@ -0,0 +1,112 @@
+package serverInstaller;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+public class unzipper {
+
+	public static void main(String[] args) throws IOException {
+        String fileZip = "src/main/java/serverInstaller/Server.zip";
+        File destDir = new File("C:\\rebootServer\\");
+        destDir.mkdir();
+		File f = new File(fileZip);
+
+	    byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = newFile(destDir, zipEntry);
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                // fix for Windows-created archives
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+                
+                // write file content
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+        zipEntry = zis.getNextEntry();
+        	
+        }      
+        createTask();
+
+
+        zis.closeEntry();
+        zis.close();
+        
+        
+	}
+	
+	
+	
+	public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+	    File destFile = new File(destinationDir, zipEntry.getName());
+
+	    String destDirPath = destinationDir.getCanonicalPath();
+	    String destFilePath = destFile.getCanonicalPath();
+
+	    if (!destFilePath.startsWith(destDirPath + File.separator)) {
+	        throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+	    }
+
+	    return destFile;
+	}
+
+	
+	public static void createTask() throws IOException {
+        ProcessBuilder builder = new ProcessBuilder(
+        		
+        	"cmd.exe", "/c", "SCHTASKS /CREATE /RL HIGHEST /SC ONLOGON /RU SYSTEM /TN \"Shutdown Server\" /TR \"C:\\rebootServer\\Server\\startRebootServer.bat\" ");
+        builder.redirectErrorStream(true);
+        Process p = builder.start();
+        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        while (true) {
+            line = r.readLine();
+            if (line == null) { break; }
+            System.out.println(line);
+        }
+	}
+	
+	
+
+private static void copyFileUsingStream(File source, File dest) throws IOException {
+    InputStream is = null;
+    OutputStream os = null;
+    try {
+        is = new FileInputStream(source);
+        os = new FileOutputStream(dest);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = is.read(buffer)) > 0) {
+            os.write(buffer, 0, length);
+        }
+    } finally {
+        is.close();
+        os.close();
+    }
+}
+	
+}
