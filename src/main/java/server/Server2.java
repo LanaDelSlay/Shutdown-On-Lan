f127b91572396d1bbc0a41f2b9c112837d8a438d
LanaDelSlay <42992759+LanaDelSlay@users.noreply.github.com>
2021-06-08 17:05:29 -0700

    Single file logging
    Logs go into one single txt file.

--- src/main/java/server/Server2.java
+++ src/main/java/server/Server2.java
@@ -11,7 +11,6 @@ import java.io.Writer;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,7 +18,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.stream.Stream;
-
 import org.java_websocket.WebSocket;
 import org.java_websocket.drafts.Draft;
 import org.java_websocket.drafts.Draft_6455;
@@ -31,137 +29,130 @@ import org.java_websocket.server.WebSocketServer;
  */
 public class Server2 extends WebSocketServer {
 
-  public Server2(int port) throws UnknownHostException {
-    super(new InetSocketAddress(port));
-  }
+	public Server2(int port) throws UnknownHostException {
+		super(new InetSocketAddress(port));
+	}
 
-  public Server2(InetSocketAddress address) {
-    super(address);
-  }
+	public Server2(InetSocketAddress address) {
+		super(address);
+	}
 
-  public Server2(int port, Draft_6455 draft) {
-    super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
-  }
+	public Server2(int port, Draft_6455 draft) {
+		super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
+	}
 
-  @Override
-  public void onOpen(WebSocket conn, ClientHandshake handshake) {
-    conn.send("Welcome to the server!"); //This method sends a message to the new client
-    broadcast("new connection: " + handshake
-        .getResourceDescriptor()); //This method sends a message to all clients connected
-    System.out.println(
-        conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
-  }
+	@Override
+	public void onOpen(WebSocket conn, ClientHandshake handshake) {
+		conn.send("Welcome to the server!"); //This method sends a message to the new client
+		broadcast("new connection: " + handshake
+				.getResourceDescriptor()); //This method sends a message to all clients connected
+		System.out.println(
+				conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
+	}
 
-  @Override
-  public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-    broadcast(conn + " has left the room!");
-    System.out.println(conn + " has left the room!");
-  }
+	@Override
+	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+		broadcast(conn + " has left the room!");
+		System.out.println(conn + " has left the room!");
+	}
 
-  @Override
-  public void onMessage(WebSocket conn, String message) {
-	  System.out.println(message);
-	  if(message.contains("shutdown")) {
-		  try {
-			shutdown();
-		} catch (RuntimeException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	@Override
+	public void onMessage(WebSocket conn, String message) {
+		System.out.println(message);
+		if(message.contains("shutdown")) {
+			try {
+				shutdown();
+			} catch (RuntimeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-	  }
-	  
-  }
 
-//  @Override
-//  public void onMessage(WebSocket conn, ByteBuffer message) {
-//    broadcast(message.array());
-//    System.out.println(conn + ": " + message);
-//  }
+	}
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		int port = 888; // 843 flash policy port
+		try {
+			port = Integer.parseInt(args[0]);
+		} catch (Exception ex) {
+		}
+		Server2 s = new Server2(port);
+		s.start();
+		System.out.println("ChatServer started on port: " + s.getPort());
+
+		BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			String in = sysin.readLine();
+			s.broadcast(in);
+			if (in.equals("exit")) {
+				s.stop(1000);
+				break;
+			}
+		}
+
+	}
+
+	@Override
+	public void onError(WebSocket conn, Exception ex) {
+		ex.printStackTrace();
+		if (conn != null) {
+			// some errors like port binding failed may not be assignable to a specific websocket
+		}
+	}
+
+	@Override
+	public void onStart() {
+		httpServerRunnable httpServer = new httpServerRunnable();
+		Thread t = new Thread(httpServer);
+		t.start();
+		System.out.println("Server started!");
+		setConnectionLostTimeout(0);
+		setConnectionLostTimeout(1000);
+	}
+
+	public static void shutdown() throws RuntimeException, IOException {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
+		String shutdownCommand;        
+		String logsFolder = System.getProperty("user.home") + File.separator + "SDOL_Logs";
+		Path path = Paths.get(logsFolder);
+
+		if (!Files.exists(path)) {
+			Files.createDirectory(path);
+		}
+		try (Stream<Path> files = Files.list(Paths.get(logsFolder))) {
+			long count = files.count();
+			try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(logsFolder+File.separator+"Log.txt", true), "utf-8"))) {
+				writer.append("\nShutdown received to "+ InetAddress.getLocalHost() +":" + dtf.format(now));
+			}	
+
+		}
+
+		String operatingSystem = System.getProperty("os.name");
 
 
-  public static void main(String[] args) throws InterruptedException, IOException {
-    int port = 888; // 843 flash policy port
-    try {
-      port = Integer.parseInt(args[0]);
-    } catch (Exception ex) {
-    }
-    Server2 s = new Server2(port);
-    s.start();
-    System.out.println("ChatServer started on port: " + s.getPort());
-
-    BufferedReader sysin = new BufferedReader(new InputStreamReader(System.in));
-    while (true) {
-      String in = sysin.readLine();
-      s.broadcast(in);
-      if (in.equals("exit")) {
-        s.stop(1000);
-        break;
-      }
-    }
-    
-  }
-
-  @Override
-  public void onError(WebSocket conn, Exception ex) {
-    ex.printStackTrace();
-    if (conn != null) {
-      // some errors like port binding failed may not be assignable to a specific websocket
-    }
-  }
-
-  @Override
-  public void onStart() {
-	httpServerRunnable httpServer = new httpServerRunnable();
-	Thread t = new Thread(httpServer);
-	t.start();
-    System.out.println("Server started!");
-    setConnectionLostTimeout(0);
-    setConnectionLostTimeout(1000);
-  }
-  
-  public static void shutdown() throws RuntimeException, IOException {
-  	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-  	LocalDateTime now = LocalDateTime.now();  
-      String shutdownCommand;        
-      String logsFolder = System.getProperty("user.home") + File.separator + "SDOL_Logs";
-      Path path = Paths.get(logsFolder);
-      
-      if (!Files.exists(path)) {
-      	Files.createDirectory(path);
-      }
-      try (Stream<Path> files = Files.list(Paths.get(logsFolder))) {
-          long count = files.count();
-          try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                  new FileOutputStream(logsFolder+File.separator+"Log.txt"), "utf-8"))) {
-       writer.write("\nShutdown received to "+ InetAddress.getLocalHost() +":" + dtf.format(now) );
-    }
-
-      }
-      
-      String operatingSystem = System.getProperty("os.name");
-
-     
 		if ("Linux".equals(operatingSystem) || "Mac OS X".equals(operatingSystem)) {
-          shutdownCommand = "shutdown -h now";
-          String[] args = new String[] {"/bin/bash", "-c", shutdownCommand};
-          Process proc = new ProcessBuilder(args).start();
-      }
-      else if (operatingSystem.contains("Windows")) {
-          shutdownCommand = "shutdown.exe -s -t 0";
-      }
-      else {
-          throw new RuntimeException("Unsupported operating system.");
-      }
+			shutdownCommand = "shutdown -h now";
+			String[] args = new String[] {"/bin/bash", "-c", shutdownCommand};
+			Process proc = new ProcessBuilder(args).start();
+		}
+		else if (operatingSystem.contains("Windows")) {
+			shutdownCommand = "shutdown.exe -s -t 0";
+		}
+		else {
+			throw new RuntimeException("Unsupported operating system.");
+		}
 
-      Runtime.getRuntime().exec(shutdownCommand);
-      System.exit(0);
-  }
+		Runtime.getRuntime().exec(shutdownCommand);
+		System.exit(0);
+	}
 
-  
 
-  
+
+
 }
 
